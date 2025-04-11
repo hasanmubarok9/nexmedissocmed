ba04b576ffb9e33332572b7913323612a58864bc
Hasan Mubarok <hasanmubarok32@gmail.com>
2025-04-11 10:44:43 +0700

    feat: install icons and init upload image

--- src/components/Tiptap.tsx
+++ src/components/Tiptap.tsx
@@ -2,11 +2,12 @@
 
 import { useEditor, EditorContent } from "@tiptap/react";
 import StarterKit from "@tiptap/starter-kit";
-import Paragraph from '@tiptap/extension-paragraph'
-import Image from '@tiptap/extension-image'
-import Text from '@tiptap/extension-text'
+import Paragraph from "@tiptap/extension-paragraph";
+import Image from "@tiptap/extension-image";
+import Text from "@tiptap/extension-text";
 import Placeholder from "@tiptap/extension-placeholder";
 import CharacterCount from "@tiptap/extension-character-count";
+import { PhotoIcon } from "@heroicons/react/24/outline";
 
 const limit = 100;
 
@@ -28,35 +29,40 @@ export default function Tiptap() {
 
   const percentage = editor
     ? Math.round((100 / limit) * editor.storage.characterCount.characters())
-    : 0
+    : 0;
 
   if (!editor) return null;
 
   return (
     <div className="w-full">
       <EditorContent editor={editor} className="w-full" />
-      <div
-        className={`character-count ${
-          editor.storage.characterCount.characters() === limit
-            ? "character-count--warning"
-            : ""
-        }`}
-      >
-        <svg height="20" width="20" viewBox="0 0 20 20">
-          <circle r="10" cx="10" cy="10" fill="#e9ecef" />
-          <circle
-            r="5"
-            cx="10"
-            cy="10"
-            fill="transparent"
-            stroke="currentColor"
-            strokeWidth="10"
-            strokeDasharray={`calc(${percentage} * 31.4 / 100) 31.4`}
-            transform="rotate(-90) translate(-20)"
-          />
-          <circle r="6" cx="10" cy="10" fill="white" />
-        </svg>
-        {editor.storage.characterCount.characters()} / {limit} characters
+      <div className="flex items-center justify-between mt-2">
+        <div
+          className={`character-count ${
+            editor.storage.characterCount.characters() === limit
+              ? "character-count--warning"
+              : ""
+          }`}
+        >
+          <svg height="20" width="20" viewBox="0 0 20 20">
+            <circle r="10" cx="10" cy="10" fill="#e9ecef" />
+            <circle
+              r="5"
+              cx="10"
+              cy="10"
+              fill="transparent"
+              stroke="currentColor"
+              strokeWidth="10"
+              strokeDasharray={`calc(${percentage} * 31.4 / 100) 31.4`}
+              transform="rotate(-90) translate(-20)"
+            />
+            <circle r="6" cx="10" cy="10" fill="white" />
+          </svg>
+          {editor.storage.characterCount.characters()} / {limit} characters
+        </div>
+        <button className="bg-gray-100 p-1 cursor-pointer rounded-md">
+          <PhotoIcon className="w-6 h-6" />
+        </button>
       </div>
     </div>
   );
