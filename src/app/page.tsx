1873a7a269dd0fc31bef6a25a4b356fcf3f8e875
Hasan Mubarok <hasanmubarok32@gmail.com>
2025-04-11 10:02:09 +0700

    feat: implement character count tiptap editor and init extension image

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,7 +7,7 @@ export default function Home() {
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <main className="max-w-sm mx-auto flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
-        <div className="flex items-center gap-3">
+        <div className="w-full flex  items-start gap-3">
           <Image
             src="https://pagedone.io/asset/uploads/1704092147.png"
             alt="Hailey Garza"
--- src/components/Tiptap.tsx
+++ src/components/Tiptap.tsx
@@ -1,21 +1,63 @@
-'use client'
+"use client";
 
-import { useEditor, EditorContent } from '@tiptap/react'
-import StarterKit from '@tiptap/starter-kit'
-import Placeholder from '@tiptap/extension-placeholder'
+import { useEditor, EditorContent } from "@tiptap/react";
+import StarterKit from "@tiptap/starter-kit";
+import Paragraph from '@tiptap/extension-paragraph'
+import Image from '@tiptap/extension-image'
+import Text from '@tiptap/extension-text'
+import Placeholder from "@tiptap/extension-placeholder";
+import CharacterCount from "@tiptap/extension-character-count";
+
+const limit = 100;
 
 export default function Tiptap() {
-    const editor = useEditor({
-        extensions: [
-            StarterKit,
-            Placeholder.configure({
-                placeholder: 'Write something...',
-            }),
-        ],        
-    })
+  const editor = useEditor({
+    extensions: [
+      Paragraph,
+      Text,
+      Image,
+      StarterKit,
+      Placeholder.configure({
+        placeholder: "Write something...",
+      }),
+      CharacterCount.configure({
+        limit,
+      }),
+    ],
+  });
 
-    return <EditorContent editor={editor} className='w-full' />
+  const percentage = editor
+    ? Math.round((100 / limit) * editor.storage.characterCount.characters())
+    : 0
+
+  if (!editor) return null;
+
+  return (
+    <div className="w-full">
+      <EditorContent editor={editor} className="w-full" />
+      <div
+        className={`character-count ${
+          editor.storage.characterCount.characters() === limit
+            ? "character-count--warning"
+            : ""
+        }`}
+      >
+        <svg height="20" width="20" viewBox="0 0 20 20">
+          <circle r="10" cx="10" cy="10" fill="#e9ecef" />
+          <circle
+            r="5"
+            cx="10"
+            cy="10"
+            fill="transparent"
+            stroke="currentColor"
+            strokeWidth="10"
+            strokeDasharray={`calc(${percentage} * 31.4 / 100) 31.4`}
+            transform="rotate(-90) translate(-20)"
+          />
+          <circle r="6" cx="10" cy="10" fill="white" />
+        </svg>
+        {editor.storage.characterCount.characters()} / {limit} characters
+      </div>
+    </div>
+  );
 }
-
-
-
