90992b978c760eebe4d7437bd2c81e0527928811
Hasan Mubarok <hasanmubarok32@gmail.com>
2025-04-12 09:51:24 +0700

    feat: init modal post detail component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,6 +6,7 @@ import { signIn, signOut, useSession } from "next-auth/react";
 import { ArrowRightEndOnRectangleIcon } from "@heroicons/react/24/outline";
 import Tooltip from "@/components/Tooltip";
 import { useGetPosts } from "@/hooks/posts";
+import PostDetail from "@/components/PostDetail";
 
 export default function Home() {
   const { data: postsData } = useGetPosts();
@@ -51,6 +52,7 @@ export default function Home() {
               })}
             />
           ))}
+          <PostDetail />
         </main>
       </div>
     );
---
+++ src/components/PostDetail.tsx
@@ -0,0 +1,152 @@
+import React, { useState, useRef, useEffect } from "react";
+import ReactDOM from "react-dom";
+import Image from "next/image";
+
+export default function ModalBasic() {
+  const [isShowing, setIsShowing] = useState(true);
+
+  const wrapperRef = useRef<HTMLDivElement>(null);
+
+  useEffect(() => {
+    function handleClickOutside(event: MouseEvent) {
+      if (
+        wrapperRef.current &&
+        !wrapperRef.current.contains(event.target as Node)
+      ) {
+        setIsShowing(false);
+      }
+    }
+    document.addEventListener("mousedown", handleClickOutside);
+    return () => {
+      document.removeEventListener("mousedown", handleClickOutside);
+    };
+  }, [wrapperRef]);
+
+  useEffect(() => {
+    function handleClickOutside(event: MouseEvent) {
+      if (
+        wrapperRef.current &&
+        !wrapperRef.current.contains(event.target as Node)
+      ) {
+        setIsShowing(false);
+      }
+    }
+    document.addEventListener("mousedown", handleClickOutside);
+    return () => {
+      document.removeEventListener("mousedown", handleClickOutside);
+    };
+  }, [wrapperRef]);
+
+  useEffect(() => {
+    let html = document.querySelector("html");
+
+    if (html) {
+      if (isShowing && html) {
+        html.style.overflowY = "hidden";
+
+        const focusableElements =
+          'button, [href], input, select, textarea, [tabindex]:not([tabindex="-1"])';
+
+        const modal = document.querySelector("#modal"); // select the modal by it's id
+
+        if (!modal) return;
+
+        const firstFocusableElement = modal.querySelectorAll(
+          focusableElements
+        )[0] as HTMLElement; // get first element to be focused inside modal
+
+        if (!firstFocusableElement) return;
+
+        const focusableContent = modal.querySelectorAll(
+          focusableElements
+        ) as NodeListOf<HTMLElement>;
+
+        const lastFocusableElement =
+          focusableContent[focusableContent.length - 1]; // get last element to be focused inside modal
+
+        document.addEventListener("keydown", function (e) {
+          if (e.keyCode === 27) {
+            setIsShowing(false);
+          }
+
+          let isTabPressed = e.key === "Tab" || e.keyCode === 9;
+
+          if (!isTabPressed) {
+            return;
+          }
+
+          if (e.shiftKey) {
+            // if shift key pressed for shift + tab combination
+            if (document.activeElement === firstFocusableElement) {
+              lastFocusableElement.focus(); // add focus for the last focusable element
+              e.preventDefault();
+            }
+          } else {
+            // if tab key is pressed
+            if (document.activeElement === lastFocusableElement) {
+              // if focused has reached to last focusable element then focus first focusable element after pressing tab
+              firstFocusableElement.focus(); // add focus for the first focusable element
+              e.preventDefault();
+            }
+          }
+        });
+
+        firstFocusableElement.focus();
+      } else {
+        html.style.overflowY = "visible";
+      }
+    }
+  }, [isShowing]);
+
+  return (
+    <>
+      <button
+        onClick={() => setIsShowing(true)}
+        className="inline-flex items-center justify-center h-10 gap-2 px-5 text-sm font-medium tracking-wide text-white transition duration-300 rounded whitespace-nowrap bg-emerald-500 hover:bg-emerald-600 focus:bg-emerald-700 focus-visible:outline-none disabled:cursor-not-allowed disabled:border-emerald-300 disabled:bg-emerald-300 disabled:shadow-none"
+      >
+        <span>Open Modal</span>
+      </button>
+
+      {isShowing && typeof document !== "undefined"
+        ? ReactDOM.createPortal(
+            <div
+              className="fixed top-0 left-0 z-20 flex items-center justify-center w-screen h-screen bg-slate-300/20 backdrop-blur-sm"
+              aria-labelledby="header-3a content-3a"
+              aria-modal="true"
+              tabIndex={-1}
+              role="dialog"
+            >
+              {/*    <!-- Modal --> */}
+              <div
+                ref={wrapperRef}
+                className="flex h-[80vh] w-9/12 max-w-full flex-col gap-6 overflow-hidden rounded bg-white p-6 text-slate-500 shadow-xl shadow-slate-700/10"
+                id="modal"
+                role="document"
+              >
+                {/*        <!-- Modal body --> */}
+                <div id="content-3a" className="flex-1 overflow-auto flex">
+                  <div className="relative h-full w-2/3">
+                    <Image
+                      src="https://fastly.picsum.photos/id/2/5000/3333.jpg?hmac=_KDkqQVttXw_nM-RyJfLImIbafFrqLsuGO5YuHqD-qQ"
+                      alt="Post Image"
+                      fill
+                      style={{ objectFit: 'cover' }}
+                    />
+                  </div>
+                  <div className="w-1/3 p-4">
+                    <p>
+                      A Terms and Conditions agreement is where you let the public
+                      know the terms, rules and guidelines for using your website
+                      or mobile app. They include topics such as acceptable use,
+                      restricted behavior and limitations of liability.
+                    </p>
+                  </div>
+                </div>
+              </div>
+            </div>,
+            document.body
+          )
+        : null}
+    </>
+  );
+}
