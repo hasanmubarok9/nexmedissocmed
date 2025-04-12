fbed4bd0b4a29b5ded28ca0f6fefb9ef324f1b1f
Hasan Mubarok <hasanmubarok32@gmail.com>
2025-04-13 06:36:38 +0700

    feat: move clientId and client secret to env

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -4,10 +4,8 @@ import GoogleProvider from "next-auth/providers/google";
 export const authOptions = {
     providers: [
         GoogleProvider({
-            // clientId: process.env.GOOGLE_CLIENT_ID,
-            // clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-            clientId: "1049941323581-aqtk1lk5qah22bhopdqupkcnnj7fbib9.apps.googleusercontent.com",
-            clientSecret: "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",
+            clientId: process.env.GOOGLE_CLIENT_ID as string,
+            clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
         }),
     ],
 };
